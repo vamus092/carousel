6827bb1e1a4d4c0058473094a020e861fd698bbb
vamus092 <dsuvam954@gmail.com>
2024-08-26 01:53:05 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Canvas } from '@react-three/fiber'
 //import React from 'react'
 import "./style.css"
+import *as THREE from"three";
 import { OrbitControls } from '@react-three/drei';
 const App = () => {
   return (
@@ -9,8 +10,8 @@ const App = () => {
       <OrbitControls/>
       <ambientLight/>
       <mesh>
-        <cylinderGeometry args={[1,1,1, 30,30, false]}/>
-        <meshStandardMaterial/>
+        <cylinderGeometry args={[1,1,1, 30,30, true]}/>
+        <meshStandardMaterial side={THREE.DoubleSide}/>
       </mesh>
     </Canvas>
   );
