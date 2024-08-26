3b37b04086820645bc9c36cbb763ec521713cde3
vamus092 <dsuvam954@gmail.com>
2024-08-26 10:53:00 +0530

    bug declutter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { Canvas } from '@react-three/fiber'
-import React from 'react'
-import "./style.css"
+import { Canvas } from "@react-three/fiber";
+import React from 'react';
+import "./style.css";
 import { OrbitControls} from '@react-three/drei';
 import Cyl from"./Cyl";
 const App = () => {
--- src/Cyl.jsx
+++ src/Cyl.jsx
@@ -5,14 +5,17 @@ import { useFrame } from "@react-three/fiber";
 
 const Cyl = () => {
     let tex=useTexture("./assets/img.png");
-  let  cyl= useRef(null)
+  let  cyl= useRef(null);
     useFrame((state,delta)=>{
 cyl.current.rotation.y+=delta;
     });
   return (
-    <mesh rotation={[0,1.4,0.5]}>    <cylinderGeometry args={[1,1,1, 60,60, true]}/>
+    <group rotation={[0,1.4,0.5]}> 
+    <mesh ref={cyl}>
+    <cylinderGeometry args={[1,1,1, 60,60, true]}/>
     <meshStandardMaterial map={tex} transparent side={THREE.DoubleSide}/>
-  </mesh>
+    </mesh>
+  </group>
   );
 };
 
