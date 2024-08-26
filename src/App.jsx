33513858e0b1b7e4e9d91a0852d32c0e4b8fc6c8
vamus092 <dsuvam954@gmail.com>
2024-08-26 11:01:55 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,25 @@ import React from 'react';
 import "./style.css";
 import { OrbitControls} from '@react-three/drei';
 import Cyl from"./Cyl";
+import { EffectComposer, ToneMapping } from "@react-three/postprocessing";
 const App = () => {
 
   return (
-    <Canvas camera={{fov:35}}>
+    <Canvas flat camera={{fov:35}}>
       <OrbitControls/>
       <ambientLight/>
      <Cyl/>
+    <EffectComposer>
+  <Bloom
+      mipmapBlur
+    intensity={7} // The bloom intensity.
+    luminanceThreshold={0} // luminance threshold. Raise this value to mask out darker elements in the scene.
+    luminanceSmoothing={0} // smoothness of the luminance threshold. Range is [0, 1]
+    // resolutionX={Resolution.AUTO_SIZE} // The horizontal resolution.
+    // resolutionY={Resolution.AUTO_SIZE} // The vertical resolution.
+  />
+  <ToneMapping adaptive/>
+</EffectComposer>
     </Canvas>
   );
 };
